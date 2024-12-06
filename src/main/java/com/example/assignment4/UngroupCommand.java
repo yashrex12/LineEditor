6a70792ce920ebc762ba7f2daff9938b8411b13a
yashrex12 <yashab128151@gmail.com>
2024-12-05 19:20:13 -0600

    New class for handling undo/redo of ungrouping

---
+++ src/main/java/com/example/assignment4/UngroupCommand.java
@@ -0,0 +1,27 @@
+package com.example.assignment4;
+
+import java.util.List;
+
+public class UngroupCommand implements DCommand{
+    LineModel model;
+    InteractionModel iModel;
+    List<Groupable> ungroupedItems;
+    Groupable group;
+
+    public UngroupCommand(LineModel model, InteractionModel iModel, Groupable group) {
+        this.model = model;
+        this.iModel = iModel;
+        this.group = group;
+    }
+    @Override
+    public void doIt() {
+        ungroupedItems = model.ungroup(group);
+    }
+
+    @Override
+    public void undo() {
+        Groupable group = model.group(ungroupedItems);
+        iModel.clearSelectedGroups();
+        iModel.selectItems(group);
+    }
+}
